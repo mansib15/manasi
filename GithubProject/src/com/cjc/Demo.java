05ed0953ecd0535d6479bfdbb36ba99d57a6c485
HP <HP@LAPTOP-7DH5S9DA>
2025-02-06 10:30:00 +0530

    commit by mansi

--- GithubProject/src/com/cjc/Demo.java
+++ GithubProject/src/com/cjc/Demo.java
@@ -4,11 +4,19 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		System.out.println("hello java selenium");
+		
+		Demo d = new Demo();
+		d.m1();
+		d.m2();
 	}
 
 	public void m1()
 	{
 		System.out.println("m1 method");
+	}
+	
+	public void m2() {
+		System.out.println("M2 Method");
 	}
 
 }
